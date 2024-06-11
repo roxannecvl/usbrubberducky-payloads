4cc5c20b685d3f4a491693bb9d33f23c270aa515
Roxanne Chevalley <125833841+roxannecvl@users.noreply.github.com>
2024-06-11 11:46:59 +0200

    Update script.py
    Change to bigger intervals and only 10 seconds

--- payloads/library/prank/The_Mouse_Moves_By_Itself/script.py
+++ payloads/library/prank/The_Mouse_Moves_By_Itself/script.py
@@ -7,9 +7,27 @@ except:
 import random
 import time
 
-for i in range(60):
-    # Move the mouse cursor randomly
-    x_offset = random.randint(-250, 250)
-    y_offset = random.randint(-250, 250)
-    pyautogui.moveRel(x_offset, y_offset, duration=0.25)
+screen_width, screen_height = pyautogui.size()
+for i in range (10):
+    # Generate random offsets
+    if random.choice([True, False]):  # Randomly choose between True (first range) and False (second range)
+        x_offset = random.randint(-500, -300)
+    else:
+        x_offset = random.randint(300, 500)
+
+    if random.choice([True, False]):  # Randomly choose between True (first range) and False (second range)
+        y_offset = random.randint(-400, -150)
+    else:
+        y_offset = random.randint(150, 400)
+
+    # Calculate the new position relative to the current position
+    new_x = pyautogui.position().x + x_offset
+    new_y = pyautogui.position().y + y_offset
+
+    # Ensure the new position stays within the screen bounds
+    new_x = max(10, min(new_x, screen_width - 10))
+    new_y = max(10, min(new_y, screen_height -10))
+
+    # Move the mouse cursor to the new position
+    pyautogui.moveTo(new_x, new_y, duration=0.25)
     time.sleep(0.1)  # 1 second delay
